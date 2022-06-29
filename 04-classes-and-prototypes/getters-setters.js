cfa59478ed8f77f79ba542cec9c325dd31542956
Guilherme Dalla Rosa <gui_dr@msn.com>
2022-06-29 10:27:43 +0100

    getters and setters example

---
+++ 04-classes-and-prototypes/getters-setters.js
@@ -0,0 +1,32 @@
+// Objects
+const myObject = {
+    price: 10,
+    quantity: 3,
+
+    get total() {
+        return this.price * this.quantity
+    },
+}
+
+console.log(myObject.total);
+
+// Classes
+class MyClass {
+    #price
+    #quantity
+
+    constructor(price, quantity) {
+        this.#price = price
+        this.#quantity = quantity
+    }
+
+    get price() { return this.#price }
+    get quantity() { return this.#quantity }
+
+    get total() {
+        return this.#price * this.quantity
+    }
+}
+
+const myClass = new MyClass(90, 2)
+console.log(myClass.total);
